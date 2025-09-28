23f926cf3d44b1e9d1fec3dc592163a9b9409446
What Can I Call Myself <WhatCanICallMyself@proton.me>
2025-09-29 01:47:10 +0200

    zmk studio working now?

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,8 +1,6 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
-CONFIG_ZMK_STUDIO=y
-
 #define MIRYOKU_LAYER_FUN \
 &kp F12,           &kp F7,            &kp F8,            &kp F9,            &kp PSCRN,         U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
 &kp F11,           &kp F4,            &kp F5,            &kp F6,            &kp SLCK,          U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
